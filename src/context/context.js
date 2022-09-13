e67f6bcad1d0cb7d2b179fa1daac084738a705ed
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-13 21:40:22 +0200

    context changes

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,9 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
-import { baseURL } from "./AppContext";
-
+import { baseURL } from "./Appcontext";
 const AuthContext = createContext()
-
 function Context(props) {
   const [user, setUser] = useState(null);
-
   const authenticateUser = async () =>{
     const token = await JSON.parse(localStorage.getItem("token"));  
     console.log("this is token ", token)
@@ -17,23 +14,16 @@ function Context(props) {
       console.log("not working")
     }
   }
-
-  // this is a change
-
   useEffect(() => { 
     // const data = localStorage.getItem("token");
     // const getUser = () => {setUser(data)} 
     // console.log("this is data ", data, "This is user", user)
     // getUser();
-
     authenticateUser();
-
   },[]);
   console.log("this is working? ",  user)
-
 //   const authenticateUser = () => {
 //     const token = localStorage.getItem('authToken'); // load the value for property with key 'authToken'
-
 //     if(token) {
 //         // update a state variable user that will carry the user information throughout the app
 //         })
@@ -48,13 +38,10 @@ function Context(props) {
 // [6:07 PM]  useEffect(() => { // each time the app loads, we check if a JWT is valid 
 //     authenticateUser();
 // }, [])
-
   return (
-
     <AuthContext.Provider value={{user, setUser}}>
       {props.children}
     </AuthContext.Provider>
   )
 }
-
 export {Context, AuthContext}
