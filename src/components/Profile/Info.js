8894d7cbc3236983bf5423b7267c1f4c04a60fab
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-14 20:58:06 +0200

    erging

--- src/components/Profile/Info.js
+++ src/components/Profile/Info.js
@@ -18,16 +18,9 @@ const toggler = () => {
 
   return (
     <div>
-<<<<<<< HEAD
-      <h3>Hello {user.username} </h3>
-      <span>Welcome to your profile page</span>
-
-
-=======
       <h3>Hello <span className='font-bold'>{user.username}</span> </h3>
       <span>Welcome to your profile page</span><br></br>
       <button onClick={toggler} className='italic'>Feel free to add your info here! </button>
->>>>>>> 04c3f64d0883c559b17c10d8969fb40c27e7687a
     </div>
   )
 }
