8528d92151e137481765a00d319ebd249402b065
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-14 09:28:17 +0200

    this is working

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { useNavigate, NavLink } from 'react-router-dom';
-import { Nav, Navbar, Container, Button, NavDropdown, LinkContainer } from "react-bootstrap";
+import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap"
 import "./NavBar.css"
 import service from '../../Context/AppContext';
 import axios from 'axios'
@@ -22,43 +23,43 @@ const NavBar = () => {
         <NavLink to="/" className="Links dark">Log In</NavLink>
         <Button onClick={doLogout}  className="Links dark btn-danger ">Log Out</Button>
 
-{/* <Navbar bg="dark" expand="lg">
-            <Container>
+        <Navbar bg="dark" expand="lg">
+                    <Container>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                      
-                            <LinkContainer to="/login">
-                                <Nav.Link>Login</Nav.Link>
-                            </LinkContainer>
-                    
-                        <LinkContainer to="/chat">
-                            <Nav.Link>Chat</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/feed">
-                            <Nav.Link>Feed</Nav.Link>
-                        </LinkContainer>
-                       
-                            <NavDropdown
-                                
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ms-auto">
+                            
+                                    <LinkContainer to="/login">
+                                        <Nav.Link>Login</Nav.Link>
+                                    </LinkContainer>
+                            
+                                <LinkContainer to="/chat">
+                                    <Nav.Link>Chat</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/feed">
+                                    <Nav.Link>Feed</Nav.Link>
+                                </LinkContainer>
+                            
+                                    <NavDropdown
+                                        
+                                        id="basic-nav-dropdown"
+                                    >
+                                        <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
 
-                                <NavDropdown.Item>
-                                    <Button variant="danger" >
-                                        Logout
-                                    </Button>
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                    
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar> */}
+                                        <NavDropdown.Item>
+                                            <Button variant="danger" >
+                                                Logout
+                                            </Button>
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                            
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
 
     </div>
   )
