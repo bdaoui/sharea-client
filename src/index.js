32a291802608927bf3c9b25359dd83521bc8ab44
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-19 13:38:58 +0200

    browser router fix

--- src/index.js
+++ src/index.js
@@ -9,16 +9,13 @@ import { Context } from './context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Context >
-    
  
+    <Context >
+     <BrowserRouter>
       <App />
-    
-    </Context>
-  
     </BrowserRouter>
-);
+    </Context>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
