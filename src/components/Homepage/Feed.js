888f276b9002d75f29ffcb0002f3193db5e9e765
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-20 10:50:09 +0200

    trying to fix main feed

--- src/components/Homepage/Feed.js
+++ src/components/Homepage/Feed.js
@@ -46,11 +46,6 @@ useEffect (() => {
 } ,[character])
 
 
-useEffect(() =>{
-  setSearching(allImages)
-  console.log("Please change ,", searching)
-}, [])
-
 
   return (
     <div className='bg-gray-50'>
@@ -70,7 +65,7 @@ useEffect(() =>{
               }}
               className="rounded-md overflow-hidden"
             />
-            <Link className='rounded-md font-amita font-black text-gray-50 text-lg' href={`/image/${item._id}`}>{item.name}
+            <Link className='rounded-md font-amita font-black text-gray-50 text-lg' to={`/image/${item._id}`}>{item.name}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 inline ml-3">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
